cc619d2c779c292bd48c65420959f85d48943d30
Torres Nicolas <sdtorres.sa@gmail.com>
2020-05-25 14:06:26 +0200

    Merge branch 'master' into devNicolas
    # Conflicts:
    #	src/td/controleur/Controleur.java
    #	src/td/modele/tir/Tir.java

--- src/td/modele/tir/TirAngle.java
+++ src/td/modele/tir/TirAngle.java
@@ -13,6 +13,9 @@ public abstract class TirAngle extends Tir {
         Position projectile = new Position(this.getX(), this.getY());
         Position cible = new Position(xCible, yCible);
         Vecteur v = new Vecteur(projectile, cible);
+        System.out.println("NORME VECTEUR : " + v.normeVecteur());
+        System.out.println(projectile.toString());
+        System.out.println(cible.toString());
         v.multiplier(3 / v.normeVecteur());
         this.dx = v.getX();
         this.dy = v.getY();
