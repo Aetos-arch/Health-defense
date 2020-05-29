d05ebd1f8d671c416814afcb3dc4530395bfe70b
Torres Nicolas <sdtorres.sa@gmail.com>
2020-05-29 16:42:57 +0200

    Plus de set

--- src/TD/Utilitaire/CoordonneProperty.java
+++ src/TD/Utilitaire/CoordonneProperty.java
@@ -4,10 +4,7 @@ import javafx.beans.property.SimpleDoubleProperty;
 
 public interface CoordonneProperty {
     SimpleDoubleProperty getXProperty();
-
     SimpleDoubleProperty getYProperty();
-
     double getX();
-
     double getY();
 }
