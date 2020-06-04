294cdecc335b4ea065251c33715202c3da912b4a
Torres Nicolas <sdtorres.sa@gmail.com>
2020-06-04 16:20:06 +0200

    Correction parametre

--- src/TD/Modele/Tourelle/TourelleViser.java
+++ src/TD/Modele/Tourelle/TourelleViser.java
@@ -1,13 +1,48 @@
 package TD.Modele.Tourelle;
 
 import TD.Modele.Environnement;
+import TD.Modele.Personnage.Personnage;
+import TD.Utilitaire.Position;
+
+import java.util.ArrayList;
 
 public class TourelleViser extends Tourelle {
+    protected int portee;
 
     public TourelleViser(int x, int y, int c, Environnement env) {
         super(x, y, 3, env);
     }
-    
+
+    public ArrayList<Personnage> estAPortee() {
+        ArrayList<Personnage> persosAPortee = new ArrayList<>();
+
+        for (Personnage p : env.getPersos()) {
+            Position positionPersoActuel = new Position(p.getX(), p.getY());
+            if (this.getPosition().distance(positionPersoActuel) <= portee) {
+                persosAPortee.add(p);
+            }
+        }
+        return persosAPortee;
+    }
+
+    public Personnage viser() {
+        if (estAPortee().size() == 0) {
+            return null;
+        }
+
+        Personnage persoPlusProche = estAPortee().get(0);
+
+        for (Personnage p : estAPortee()) {
+            Position positionPersoProche = new Position(persoPlusProche.getX(), persoPlusProche.getY());
+            Position positionPersoActuel = new Position(p.getX(), p.getY());
+
+            if (this.getPosition().distance(positionPersoProche) > this.getPosition().distance(positionPersoActuel)) {
+                persoPlusProche = p;
+            }
+        }
+        return persoPlusProche;
+    }
+
     @Override
     public void agit() {
 
--- src/TD/Modele/Tourelle/TourelleVitamine.java
+++ src/TD/Modele/Tourelle/TourelleVitamine.java
@@ -6,15 +6,11 @@ import TD.Modele.Tir.Tir;
 import TD.Modele.Tir.TirVitamine;
 import TD.Utilitaire.Position;
 
-import java.util.ArrayList;
-
-public class TourelleVitamine extends Tourelle {
-    private int portee;
+public class TourelleVitamine extends TourelleViser {
     private int delai;
 
     public TourelleVitamine(int x, int y, Environnement env) {
         super(x, y, 3, env);
-        this.portee = 100;
         this.delai = 0;
     }
 
@@ -24,41 +20,11 @@ public class TourelleVitamine extends Tourelle {
             Personnage p = viser();
             if (p != null) {
                 Position positionCible = new Position(p.getX() + 8, p.getY() + 8);
-                Tir tir = new TirVitamine(this.getPosition(), positionCible, env, this);
+                Tir tir = new TirVitamine(positionCible, env, this);
                 env.ajouterTir(tir);
             }
         }
         delai++;
-    }
-
-    public ArrayList<Personnage> estAPortee() {
-        ArrayList<Personnage> persosAPortee = new ArrayList<>();
-
-        for (Personnage p : env.getPersos()) {
-            Position positionPersoActuel = new Position(p.getX(), p.getY());
-            if (this.getPosition().distance(positionPersoActuel) <= portee) {
-                persosAPortee.add(p);
-            }
-        }
-        return persosAPortee;
-    }
-
-    public Personnage viser() {
-        if (estAPortee().size() == 0) {
-            return null;
-        }
-
-        Personnage persoPlusProche = estAPortee().get(0);
-
-        for (Personnage p : estAPortee()) {
-            Position positionPersoProche = new Position(persoPlusProche.getX(), persoPlusProche.getY());
-            Position positionPersoActuel = new Position(p.getX(), p.getY());
-
-            if (this.getPosition().distance(positionPersoProche) > this.getPosition().distance(positionPersoActuel)) {
-                persoPlusProche = p;
-            }
-        }
-        return persoPlusProche;
     }
 
     public int getPortee() {
