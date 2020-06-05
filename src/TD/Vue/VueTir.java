557ba4f118ceba5afed2df6dd6d3a3c8ecfb7932
Torres Nicolas <sdtorres.sa@gmail.com>
2020-06-05 11:14:35 +0200

    TourelleSeringue fini + TirSeringue fini + Images OK

--- src/TD/Vue/VueTir.java
+++ src/TD/Vue/VueTir.java
@@ -4,7 +4,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class VueTir extends ImageView {
-    private final String[] URL = {"Sources/Tirs/ball.png",};
+    private final String[] URL = {"Sources/Tirs/ball.png", "Sources/Tirs/TirSeringue.png"};
 
     public VueTir(int url) {
         super();
