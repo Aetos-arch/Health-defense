487d0c365ee0b93ceaa9f5e6bf91d004ea39f2fd
Torres Nicolas <sdtorres.sa@gmail.com>
2020-06-08 17:34:06 +0200

    Image Hot dans package tir

--- src/TD/Vue/VueHoT.java
+++ src/TD/Vue/VueHoT.java
@@ -6,6 +6,6 @@ import javafx.scene.image.ImageView;
 public class VueHoT extends ImageView {
 
 	public VueHoT() {
-		this.setImage(new Image("Sources/Hot.png"));
+		this.setImage(new Image("Sources/Tirs/Hot.png"));
 	}
 }
