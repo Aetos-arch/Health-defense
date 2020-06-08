f1cc1be5ccbffe3e1e07c067b2240f890ed96a76
Viincii <vinc.mignot@hotmail.fr>
2020-06-08 11:23:54 +0200

    Ajout de l'affichage du heal on time

---
+++ src/TD/Vue/VueHoT.java
@@ -0,0 +1,14 @@
+package TD.Vue;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class VueHoT extends ImageView {
+
+	public VueHoT() {
+		this.setImage(new Image("Sources/HoT.png"));
+		Rectangle2D rogne = new Rectangle2D(56, 16, 16, 16);
+		this.setViewport(rogne);
+	}
+}
