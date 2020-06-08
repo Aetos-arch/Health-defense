42073bbba3d1fe3b920b67c9e0e61d57cfaec47b
Viincii <vinc.mignot@hotmail.fr>
2020-06-08 16:14:28 +0200

    Changement de la hitbox pour ne pas touché les perso sains

--- src/TD/Modele/Tir/TirDirection.java
+++ src/TD/Modele/Tir/TirDirection.java
@@ -28,8 +28,8 @@ public abstract class TirDirection extends Tir {
     @Override
     public boolean collision() {
         for (Personnage p : this.env.getPersos()) {
-            if ((p.getY() >= this.getY() - hitbox && p.getY() <= this.getY() + hitbox) &&
-                    (p.getX() >= this.getX() - hitbox && p.getX() <= this.getX() + hitbox)) {
+            if (((p.getY() >= this.getY() - hitbox && p.getY() <= this.getY() + hitbox) &&
+                    (p.getX() >= this.getX() - hitbox && p.getX() <= this.getX() + hitbox))&& !p.estSain()) {
                 p.seFaireSoigner(this.pointAttaque);
                 return true;
             }
